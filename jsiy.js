c807430c9ff7c052d0dd96d0cf51c82f65cc0217
And-col <76390265+And-col@users.noreply.github.com>
2021-05-30 12:51:11 +0300

    Add files via upload

---
+++ jsiy.js
@@ -0,0 +1,12 @@
+tag = document.getElementById("a")
+
+
+function white_col() {
+    tag.style.backgroundColor = "white"
+}
+
+function change_color() {
+    tag.style.backgroundColor = "#f5720f"
+}
+
+
